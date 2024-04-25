cb8aa230417689b546298649fff90053baf26c5c
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2024-04-25 22:20:28 +0530

    Modified style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ const Header = () => {
   return (
     <div className='flex flex-row justify-between items-center shadow-lg bg-[color:var(--header-bg--color)]'>
         {/* Logo */}
-        <div className='flex flex-row gap-x-5 p-4 ml-16 text-2xl font-bold text-[color:var(--primary-text--color)]'>
+        <div className='flex flex-row gap-x-5 p-4 ml-[7%] text-2xl font-bold text-[color:var(--primary-text--color)]'>
             <img src={Jira} className='h-10 w-10'></img>
             <p>Kanban Board</p>
         </div>
         {/* Navbar Items */}
-        <div className='p-4 mr-16'>
+        <div className='p-4 mr-[7%]'>
             <ul className='flex flex-row gap-x-16 font-semibold text-xl text-[color:var(--secondary-text--color)]'>
                 <li className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer'>Home</li>
                 <li className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer'>Features</li>
