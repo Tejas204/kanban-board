5d9ba27e43b3d89bfc0ac47ecfbd836aabba07d1
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2024-04-26 22:43:23 +0530

    Designed login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex h-[100%] w-[100%] justify-center items-center'>
+      {/* Login Form */}
+        <div className='flex flex-col rounded-md mt-[3%] pt-10 pb-10 pl-14 pr-14 gap-y-14 shadow-lg bg-[color:var(--card-bg--color)]'>
+          <div className='flex flex-col items-center'>
+            <p className='text-4xl'>Get Started</p>
+            <p className='text-xl'>Free for users, no credit card needed</p>
+          </div>
+          
+          <form className='flex flex-col gap-y-5'>
+            <input type='text' placeholder='Email' className='p-4 rounded-md bg-[color:var(--board-bg--color)] border-[0.1rem] text-lg'></input>
+            <input type='password' placeholder='Password' className='p-4 rounded-md bg-[color:var(--board-bg--color)] border-[0.1rem] text-lg'></input>
+            <button type='button' className='p-4 bg-[color:var(--button-bg--color)] text-[color:var(--button-text--color)] text-lg font-bold rounded-md'>Log In</button>
+          </form>
+          
+          <div className='flex flex-col items-center'>
+            <p className='text-lg font-semibold'>Or sign in with</p>
+          </div>
+          
+          <div className='grid grid-cols-2 gap-x-3 gap-y-2 '>
+            <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md hover:bg-[color:var(--button-bg--color)] transition delay-100 ease-in-out'>Google</div>
+            <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md hover:bg-[color:var(--button-bg--color)] transition delay-100 ease-in-out'>Microsoft</div>
+            <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md hover:bg-[color:var(--button-bg--color)] transition delay-100 ease-in-out'>Apple</div>
+            <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md hover:bg-[color:var(--button-bg--color)] transition delay-100 ease-in-out'>Slack</div>
+          </div>
+        </div>
+    </div>
   )
 }
 
