75cdf6ffe1c876d29bbb6c48e418b180a220fbbe
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2024-04-28 11:23:08 +0530

    Modified Logo and Links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import Jira from '../assets/Jira.png';
+import Kanbanize from '../assets/Kanbanize-Logo.png'
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className='flex flex-row justify-between items-center shadow-lg bg-[color:var(--header-bg--color)]'>
         {/* Logo */}
-        <div className='flex flex-row gap-x-5 p-4 ml-[7%] text-2xl font-bold text-[color:var(--primary-text--color)]'>
-            <img src={Jira} className='h-10 w-10'></img>
+        <Link className='flex flex-row gap-x-5 p-4 ml-[7%] text-2xl items-center font-bold text-[color:var(--primary-text--color)]' to='/'>
+            <img src={Kanbanize} className='h-10 w-10'></img>
             <p>Kanbanize</p>
-        </div>
+        </Link>
         {/* Navbar Items */}
         <div className='p-4 mr-[7%]'>
             <ul className='flex flex-row gap-x-16 font-semibold text-xl text-[color:var(--secondary-text--color)]'>
-                <Link to='/' className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer'>Home</Link>
-                <Link to='/' className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer'>Features</Link>
-                <Link to='/login' className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer'>Login</Link>
-                <Link to='/register' className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer'>Register</Link>
+                <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/'>Home</Link>
+                <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/' >Features</Link>
+                <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/login' >Login</Link>
+                <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/register' >Register</Link>
             </ul>
         </div>
     </div>
