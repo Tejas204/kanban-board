2d295cb8b38c4d5fede8c62a99a53560f1ad0449
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2024-04-29 22:33:42 +0530

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Kanbanize from '../assets/Kanbanize-Logo.png'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
     // Footer Skeleton
-    <div className='grid grid-cols-4 gap-x-2 w-[100%] mt-[5%] py-14 px-10 bg-[color:var(--header-bg--color)]'>
+    <div className='grid grid-cols-3 gap-x-2 w-[100%] mt-[5%] py-14 px-10 bg-[color:var(--header-bg--color)]'>
         {/* Footer Hero Section */}
-        <div className='flex flex-col gap-y-6'>
+        <div className='flex flex-col gap-y-6 px-6'>
             {/* Logo */}
             <img src={Kanbanize} alt="" className="h-24 w-24" />
 
@@ -14,7 +15,52 @@ const Footer = () => {
             <p className='text-lg'>Kanbanize lets you create kanban boards to help you manage your team, tasks and projects better. Use the interactive board, prioritize your tasks and make your project a success.</p>
 
             {/* Privacy statement and Copyright */}
-            <p>&copy 2024 Kanbanize IND. All rights reserved</p>
+            <p>&copy; 2024 Kanbanize IND. All rights reserved</p>
+        </div>
+
+        {/* Menu and Social Media */}
+        <div className='grid grid-cols-2'>
+            {/* Menu */}
+            <div className='flex flex-col gap-y-6 px-6 border-l-[0.1rem] border-[color:var(--secondary-text--color)]'>
+                {/* Menu Header */}
+                <p className='text-xl font-bold'>Menu</p>
+
+                {/* Menu item list */}
+                <ul className='flex flex-col gap-y-8 text-lg text-[color:var(--secondary-text--color)]'>
+                    <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/'>Home</Link>
+                    <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/' >Features</Link>
+                    <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/login' >Login</Link>
+                    <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/register' >Register</Link>
+                </ul>
+            </div>
+
+            {/* Social Media List */}
+            <div className='flex flex-col gap-y-6 px-6 border-l-[0.1rem] border-[color:var(--secondary-text--color)]'>
+                {/* Social Media */}
+                <p className='text-xl font-bold'>Follow us on</p>
+
+                {/* Menu item list */}
+                <ul className='flex flex-col gap-y-8 text-lg text-[color:var(--secondary-text--color)]'>
+                    <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/'>LinkedIn</Link>
+                    <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/' >Instagram</Link>
+                    <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/login' >Twitter</Link>
+                    <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/register' >Facebook</Link>
+                </ul>
+            </div>
+        </div>
+
+        {/* Contact Us */}
+        <div className='flex flex-col gap-y-6 px-6 border-l-[0.1rem] border-[color:var(--secondary-text--color)]'>
+            {/* Call to action */}
+            <p className='text-xl font-bold'>Want an improved experience?</p>
+
+            {/* Text area */}
+            <form className='flex flex-col gap-y-8'>
+                <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/'>LinkedIn</Link>
+                <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/' >Instagram</Link>
+                <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/login' >Twitter</Link>
+                <Link className='hover:text-[color:var(--primary-text--color)] transition delay-100 ease-in-out cursor-pointer' to='/register' >Facebook</Link>
+            </form>
         </div>
     </div>
   )
