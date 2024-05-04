3d06d79bfb379deb9f8eed6557fa524332bd220f
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2024-05-04 20:06:20 +0530

    Completed card design

--- src/pages/TestCard.jsx
+++ src/pages/TestCard.jsx
@@ -7,7 +7,6 @@ const TestCard = () => {
   return (
     <div className='flex flex-col space-y-10 mt-[10%] ml-[10%]'>
         <Card priority={priority}></Card>
-        <Card priority={priority}></Card>
     </div>
   )
 }
