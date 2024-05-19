3e6cbd217a9682416ce38690ab905388e42839cf
Tejas Dhopavkar <tejasdhopavkar2000@gmail.com>
2024-05-19 19:58:10 +0530

    Added Footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import UI from '../assets/UI.png';
+import Footer from '../components/Footer';
 
 const Home = () => {
 
@@ -8,25 +9,30 @@ const Home = () => {
                    </svg>
 
   return (
-    <div className='grid grid-cols-2 ml-[8%] mt-[5%]'>
-      {/* Capmaign Div */}
-      <div className='flex flex-col gap-y-9 text-[color:var(--primary-text--color)] w-[70%]'>
-        <p className='text-6xl'>Create a Kanban board for your team</p>
-        <p className='text-3xl'>Start your project off right with a board build for all teams</p>
-        <p className='text-xl font-semibold'>Included in your free plan:</p>
-        <ul className='pl-4'>
-          <li className='flex flex-row gap-x-5 text-xl pb-9'>{tickmark} 10 user licenses</li>
-          <li className='flex flex-row gap-x-5 text-xl pb-9'>{tickmark} Workflows and automation</li>
-          <li className='flex flex-row gap-x-5 text-xl pb-9'>{tickmark} Project templates - including scrum, kanban, and more</li>
-          <li className='flex flex-row gap-x-5 text-xl pb-9'>{tickmark} Integrations with thousands of other tools</li>
-        </ul>
-      </div>
+    // Parent div
+    <div>
+        <div className='grid grid-cols-2 ml-[8%] mt-[5%]'>
+          {/* Capmaign Div */}
+          <div className='flex flex-col gap-y-9 text-[color:var(--primary-text--color)] w-[70%]'>
+            <p className='text-6xl'>Create a Kanban board for your team</p>
+            <p className='text-3xl'>Start your project off right with a board build for all teams</p>
+            <p className='text-xl font-semibold'>Included in your free plan:</p>
+            <ul className='pl-4'>
+              <li className='flex flex-row gap-x-5 text-xl pb-9'>{tickmark} 10 user licenses</li>
+              <li className='flex flex-row gap-x-5 text-xl pb-9'>{tickmark} Workflows and automation</li>
+              <li className='flex flex-row gap-x-5 text-xl pb-9'>{tickmark} Project templates - including scrum, kanban, and more</li>
+              <li className='flex flex-row gap-x-5 text-xl pb-9'>{tickmark} Integrations with thousands of other tools</li>
+            </ul>
+          </div>
 
-      {/* Form Div */}
-      <div className='w-[100%]'>
-        <img src={UI} className='w-[90%] shadow-2xl border-[0.1rem] border-[color:var(--user-icon--bg-color--lavender)]'></img>
-      </div>
-      
+          {/* Form Div */}
+          <div className='w-[100%]'>
+            <img src={UI} className='w-[90%] shadow-2xl border-[0.1rem] border-[color:var(--user-icon--bg-color--lavender)]'></img>
+          </div>
+        </div>
+    
+    {/* Footer */}
+    <Footer></Footer>
     </div>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,40 +1,47 @@
 import React from 'react'
+import Footer from '../components/Footer'
 
 const Login = () => {
   return (
-    <div className='flex h-[100%] w-[100%] justify-center items-center'>
-      {/* Title */}
-        <div className='flex flex-col rounded-md mt-[3%] py-10 px-16 gap-y-14 shadow-xl bg-[color:var(--card-bg--color)]'>
-          <div className='flex flex-col items-center gap-y-2'>
-            <p className='text-4xl'>Get Started</p>
-            <p className='text-xl'>Free for users, no credit card needed</p>
+    // Parent div
+    <div>
+      <div className='flex h-[100%] w-[100%] justify-center items-center'>
+        {/* Title */}
+          <div className='flex flex-col rounded-md mt-[3%] py-10 px-16 gap-y-14 shadow-xl bg-[color:var(--card-bg--color)]'>
+            <div className='flex flex-col items-center gap-y-2'>
+              <p className='text-4xl'>Get Started</p>
+              <p className='text-xl'>Free for users, no credit card needed</p>
+            </div>
+            
+            {/* Login Form */}
+            <form className='flex flex-col gap-y-5'>
+              <input className='p-4 rounded-md bg-[color:var(--board-bg--color)] border-[0.1rem] text-lg' type='text' placeholder='Email' ></input>
+              <input className='p-4 rounded-md bg-[color:var(--board-bg--color)] border-[0.1rem] text-lg' type='password' placeholder='Password' ></input>
+              <button className='p-4 bg-[color:var(--button-bg--color)] text-[color:var(--button-text--color)] text-lg font-bold rounded-md 
+                  hover:ring-2 ring-[color:var(--button-bg--color)] ring-offset-4 ring-offset-[color:var(--card-bg--color)] transition delay-150 ease-in-out' type='button' >
+                    Log In
+              </button>
+            </form>
+            
+            {/* Other Sign in options text */}
+            <div className='flex flex-row justify-center items-center gap-x-2'>
+              <hr className='w-24 bg-[color:var(--secondary-text--color)]'></hr>
+              <p className='text-lg font-semibold'>Or sign in with the help of</p>
+              <hr className='w-24'></hr>
+            </div>
+            
+            {/* Other sign in options */}
+            <div className='grid grid-cols-2 gap-x-4 gap-y-7'>
+              <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md shadow-lg hover:bg-[color:var(--button-bg--color)] hover:border-[color:var(--button-bg--color)] transition delay-100 ease-in-out cursor-pointer'>Google</div>
+              <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md shadow-lg hover:bg-[color:var(--button-bg--color)] hover:border-[color:var(--button-bg--color)] transition delay-100 ease-in-out cursor-pointer'>Microsoft</div>
+              <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md shadow-lg hover:bg-[color:var(--button-bg--color)] hover:border-[color:var(--button-bg--color)] transition delay-100 ease-in-out cursor-pointer'>Apple</div>
+              <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md shadow-lg hover:bg-[color:var(--button-bg--color)] hover:border-[color:var(--button-bg--color)] transition delay-100 ease-in-out cursor-pointer'>Slack</div>
+            </div>
           </div>
-          
-          {/* Login Form */}
-          <form className='flex flex-col gap-y-5'>
-            <input className='p-4 rounded-md bg-[color:var(--board-bg--color)] border-[0.1rem] text-lg' type='text' placeholder='Email' ></input>
-            <input className='p-4 rounded-md bg-[color:var(--board-bg--color)] border-[0.1rem] text-lg' type='password' placeholder='Password' ></input>
-            <button className='p-4 bg-[color:var(--button-bg--color)] text-[color:var(--button-text--color)] text-lg font-bold rounded-md 
-                hover:ring-2 ring-[color:var(--button-bg--color)] ring-offset-4 ring-offset-[color:var(--card-bg--color)] transition delay-150 ease-in-out' type='button' >
-                  Log In
-            </button>
-          </form>
-          
-          {/* Other Sign in options text */}
-          <div className='flex flex-row justify-center items-center gap-x-2'>
-            <hr className='w-24 bg-[color:var(--secondary-text--color)]'></hr>
-            <p className='text-lg font-semibold'>Or sign in with the help of</p>
-            <hr className='w-24'></hr>
-          </div>
-          
-          {/* Other sign in options */}
-          <div className='grid grid-cols-2 gap-x-4 gap-y-7'>
-            <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md shadow-lg hover:bg-[color:var(--button-bg--color)] hover:border-[color:var(--button-bg--color)] transition delay-100 ease-in-out cursor-pointer'>Google</div>
-            <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md shadow-lg hover:bg-[color:var(--button-bg--color)] hover:border-[color:var(--button-bg--color)] transition delay-100 ease-in-out cursor-pointer'>Microsoft</div>
-            <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md shadow-lg hover:bg-[color:var(--button-bg--color)] hover:border-[color:var(--button-bg--color)] transition delay-100 ease-in-out cursor-pointer'>Apple</div>
-            <div className='p-2 border-[0.1rem] text-center font-semibold rounded-md shadow-lg hover:bg-[color:var(--button-bg--color)] hover:border-[color:var(--button-bg--color)] transition delay-100 ease-in-out cursor-pointer'>Slack</div>
-          </div>
-        </div>
+      </div>
+
+      {/* Footer */}
+    <Footer></Footer>
     </div>
 
     
