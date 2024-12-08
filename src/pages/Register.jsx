76c9b2b3169dbdc9a1a8b9ab348225cfcb4139a1
Tejas204 <tejasdhopavkar2000@gmail.com>
2024-12-08 15:17:20 +0530

    Hooks for registration

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,38 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registrationHandler = (event) => {
+    event.preventDefault();
+    console.log(name, email, password);
+  };
+
   return (
-    // Parent Div
     <div>
       <div className="flex h-[100%] w-[100%] justify-center items-center">
-        {/* Login Form */}
+        {/* Registration Form */}
         <div className="flex flex-col rounded-md mt-[3%] py-10 px-16 gap-y-14 shadow-xl text-[color:var(--primary-text--color)] bg-[color:var(--card-bg--color)]">
           <div className="flex flex-col items-center gap-y-2">
             <p className="text-4xl">Register Now</p>
             <p className="text-xl">Registration is free for all users</p>
           </div>
 
-          <form className="flex flex-col gap-y-5">
+          <form
+            className="flex flex-col gap-y-5"
+            onSubmit={registrationHandler}
+          >
             <input
               className="p-4 rounded-md bg-[color:var(--board-bg--color)] border-[0.1rem] text-lg"
               type="text"
               placeholder="Name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
             ></input>
             <input
               className="p-4 rounded-md bg-[color:var(--board-bg--color)] border-[0.1rem] text-lg"
               type="text"
               placeholder="Email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
             ></input>
             <input
               className="p-4 rounded-md bg-[color:var(--board-bg--color)] border-[0.1rem] text-lg"
               type="password"
               placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
             ></input>
             <button
               className="p-4 bg-[color:var(--button-bg--color)] text-[color:var(--button-text--color)] text-lg font-bold rounded-md hover:ring-2 ring-[color:var(--button-bg--color)] ring-offset-4 ring-offset-[color:var(--card-bg--color)] transition delay-150 ease-in-out"
-              type="button"
+              type="submit"
             >
               Register
             </button>
