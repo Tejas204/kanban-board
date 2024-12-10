04e6eb21951a0005f98e48d01563d2f7ec9fac8c
Tejas204 <tejasdhopavkar2000@gmail.com>
2024-12-10 23:10:02 +0530

    Login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,34 +1,76 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Footer from "../components/Footer";
+import axios from "axios";
+import { Context, server } from "../main";
+import toast from "react-hot-toast";
 
 const Login = () => {
+  /**
+   * @Hooks: For setting name, email and authentication check
+   */
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
+
+  /**
+   * @Function: handleLogin
+   * @Params: event
+   * Login handler function to make API call
+   */
+  const handleLogin = async (event) => {
+    event.preventDefault();
+
+    try {
+      const data = await axios.post(
+        `${server}/users/login`,
+        {
+          email,
+          password,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+          withCredentials: true,
+        }
+      );
+
+      toast.success(data.message);
+      setIsAuthenticated(true);
+    } catch (error) {
+      toast.error("Login error");
+      setIsAuthenticated(false);
+    }
+  };
+
   return (
-    // Parent div
     <div>
       <div className="flex h-[100%] w-[100%] justify-center items-center">
-        {/* Title */}
         <div className="flex flex-col rounded-md mt-[3%] py-10 px-16 gap-y-14 shadow-xl text-[color:var(--primary-text--color)] bg-[color:var(--card-bg--color)]">
           <div className="flex flex-col items-center gap-y-2">
             <p className="text-4xl">Get Started</p>
             <p className="text-xl">Free for users, no credit card needed</p>
           </div>
 
-          {/* Login Form */}
-          <form className="flex flex-col gap-y-5">
+          <form className="flex flex-col gap-y-5" onSubmit={handleLogin}>
             <input
               className="p-4 rounded-md bg-[color:var(--board-bg--color)] border-[0.1rem] text-lg"
               type="text"
               placeholder="Email"
+              value={email}
+              onChange={(event) => setEmail(event.target.value)}
             ></input>
             <input
               className="p-4 rounded-md bg-[color:var(--board-bg--color)] border-[0.1rem] text-lg"
               type="password"
               placeholder="Password"
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
             ></input>
             <button
               className="p-4 bg-[color:var(--button-bg--color)] text-[color:var(--button-text--color)] text-lg font-bold rounded-md 
                   hover:ring-2 ring-[color:var(--button-bg--color)] ring-offset-4 ring-offset-[color:var(--card-bg--color)] transition delay-150 ease-in-out"
-              type="button"
+              type="submit"
             >
               Log In
             </button>
