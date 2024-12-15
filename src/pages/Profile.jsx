46d707e85aef8cba13125beeec9c9a66290a399a
Tejas204 <tejasdhopavkar2000@gmail.com>
2024-12-15 11:56:50 +0530

    Profile Page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,19 +4,23 @@ import { Context } from "../main";
 const Profile = () => {
   const { isAuthenticated, user } = useContext(Context);
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col gap-y-10 w-1/2 h-1/4 bg-[color:var(--card-bg--color)] shadow-xl rounded-lg text-[color:var(--primary-text--color)] text-xl pb-3">
+    <div className="flex h-fit mt-60 items-center justify-center">
+      <div className="flex flex-col gap-y-10 w-1/3 bg-[color:var(--card-bg--color)] shadow-xl rounded-lg text-[color:var(--primary-text--color)] text-xl pb-10">
         <div className="text-center text-3xl font-semibold pt-3">
           Your profile
         </div>
         <div className="grid grid-cols-2 gap-x-1 gap-y-5 px-3">
-          <div className="px-10 py-2 text-2xl">Name</div>
+          <div className="px-10 py-2 text-2xl">Name:</div>
           <div className="px-10 py-2 text-2xl border-b-2 border-b-[color:var(--secondary-text--color)]">
             {user.name}
           </div>
-          <div className="px-10 py-2 text-2xl">Email</div>
+          <div className="px-10 py-2 text-2xl">Email:</div>
           <div className="px-10 py-2 text-2xl border-b-2 border-b-[color:var(--secondary-text--color)]">
             {user.email}
+          </div>
+          <div className="px-10 py-2 text-2xl">Joined us on:</div>
+          <div className="px-10 py-2 text-2xl border-b-2 border-b-[color:var(--secondary-text--color)]">
+            {user.createdAt}
           </div>
         </div>
       </div>
