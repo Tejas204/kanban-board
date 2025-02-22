f5065a4b469868814bf9645c625363cf7df0b3bb
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-02-22 11:37:44 +0530

    UI Changes

--- src/components/UpdateDeleteCardModal.jsx
+++ src/components/UpdateDeleteCardModal.jsx
@@ -343,83 +343,29 @@ const UpdateDeleteCardModal = ({ updateDeleteCard, hideModal }) => {
 
           {/* Existing comments */}
           <div className="flex flex-col gap-y-3 w-[100%]">
-            <div className="grid grid-cols-10 w-[100%] p-3 bg-gray-100 rounded-lg">
-              <div className="col-span-1">
-                <div className="flex items-center font-semibold h-11 w-11 p-3 rounded-full text-[color:var(--primary-dark--text-color)] bg-orange-400">
-                  TD
+            {comments.map((comment) => {
+              return (
+                <div
+                  key={comment._id}
+                  className="grid grid-cols-10 w-[100%] p-3 bg-gray-100 rounded-lg"
+                >
+                  <div className="col-span-1">
+                    <div className="flex items-center font-semibold h-11 w-11 p-3 rounded-full text-[color:var(--primary-dark--text-color)] bg-orange-400">
+                      TD
+                    </div>
+                  </div>
+                  <div className="col-span-9 flex flex-col gap-y-2">
+                    <div className="font-semibold flex flex-row justify-between w-[100%]">
+                      <div>Tejas Dhopavkar</div>
+                      <button className="text-gray-500">
+                        {deleteCommentIcon}
+                      </button>
+                    </div>
+                    <div className="text-justify">{comment.comment}</div>
+                  </div>
                 </div>
-              </div>
-              <div className="col-span-9 flex flex-col gap-y-2">
-                <div className="font-semibold flex flex-row justify-between w-[100%]">
-                  <div>Tejas Dhopavkar</div>
-                  <button className="text-gray-500">{deleteCommentIcon}</button>
-                </div>
-                <div className="text-justify">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. It has survived not only five centuries, but
-                  also the leap into electronic typesetting, remaining
-                  essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum
-                  passages, and more recently with desktop publishing software
-                  like Aldus PageMaker including versions of Lorem Ipsum.
-                </div>
-              </div>
-            </div>
-
-            <div className="grid grid-cols-10 w-[100%] p-3 bg-gray-100 rounded-lg">
-              <div className="col-span-1">
-                <div className="flex items-center font-semibold h-11 w-11 p-3 rounded-full text-[color:var(--primary-dark--text-color)] bg-orange-400">
-                  TD
-                </div>
-              </div>
-              <div className="col-span-9 flex flex-col gap-y-2">
-                <div className="font-semibold flex flex-row justify-between w-[100%]">
-                  <div>Tejas Dhopavkar</div>
-                  <button className="text-gray-500">{deleteCommentIcon}</button>
-                </div>
-                <div className="text-justify">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. It has survived not only five centuries, but
-                  also the leap into electronic typesetting, remaining
-                  essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum
-                  passages, and more recently with desktop publishing software
-                  like Aldus PageMaker including versions of Lorem Ipsum.
-                </div>
-              </div>
-            </div>
-
-            <div className="grid grid-cols-10 w-[100%] p-3 bg-gray-100 rounded-lg">
-              <div className="col-span-1">
-                <div className="flex items-center font-semibold h-11 w-11 p-3 rounded-full text-[color:var(--primary-dark--text-color)] bg-orange-400">
-                  TD
-                </div>
-              </div>
-              <div className="col-span-9 flex flex-col gap-y-2">
-                <div className="font-semibold flex flex-row justify-between w-[100%]">
-                  <div>Tejas Dhopavkar</div>
-                  <button className="text-gray-500">{deleteCommentIcon}</button>
-                </div>
-                <div className="text-justify">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry's
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it to make a type
-                  specimen book. It has survived not only five centuries, but
-                  also the leap into electronic typesetting, remaining
-                  essentially unchanged. It was popularised in the 1960s with
-                  the release of Letraset sheets containing Lorem Ipsum
-                  passages, and more recently with desktop publishing software
-                  like Aldus PageMaker including versions of Lorem Ipsum.
-                </div>
-              </div>
-            </div>
+              );
+            })}
           </div>
         </div>
       </div>
