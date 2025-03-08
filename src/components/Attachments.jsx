02ea9e3cf011593c7316ffedd9af543b037bb7c7
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-03-08 20:20:14 +0530

    Attachment Component

---
+++ src/components/Attachments.jsx
@@ -0,0 +1,79 @@
+import UIImage from "../assets/Kanban_Board_UI.png";
+import { deleteCommentIcon, downloadIcon } from "../data/icons";
+
+const Attachments = () => {
+  return (
+    <div className="flex flex-col gap-y-3">
+      <div className="text-3xl font-semibold text-[color:var(--board-bg--color)]">
+        Attachments
+      </div>
+      <div className="grid grid-cols-3 gap-x-2 gap-y-4">
+        <div className="flex flex-row justify-between px-5 py-3 rounded-lg shadow-lg bg-gray-200">
+          <div className="w-[80%] overflow-hidden text-ellipsis line-clamp-1">
+            Attachment Attachment Attachment
+          </div>
+          <div className="flex flex-row items-center gap-x-2">
+            <a href={UIImage} download="Example">
+              <button
+                className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
+              >
+                {downloadIcon}
+              </button>
+            </a>
+
+            <button
+              onClick={() => alert("Attachment is deleted")}
+              className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
+            >
+              {deleteCommentIcon}
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-row justify-between px-5 py-3 rounded-lg shadow-lg bg-gray-200">
+          <div className="w-[80%] overflow-hidden text-ellipsis line-clamp-1">
+            Attachment 2
+          </div>
+          <div className="flex flex-row items-center gap-x-2">
+            <a href={UIImage} download="Example">
+              <button
+                className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
+              >
+                {downloadIcon}
+              </button>
+            </a>
+            <button
+              onClick={() => alert("Attachment is deleted")}
+              className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
+            >
+              {deleteCommentIcon}
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-row justify-between px-5 py-3 rounded-lg shadow-lg bg-gray-200">
+          <div className="w-[80%] overflow-hidden text-ellipsis line-clamp-1">
+            Attachment 3
+          </div>
+          <div className="flex flex-row items-center gap-x-2">
+            <a href={UIImage} download="Example">
+              <button
+                className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
+              >
+                {downloadIcon}
+              </button>
+            </a>
+            <button
+              onClick={() => alert("Attachment is deleted")}
+              className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
+            >
+              {deleteCommentIcon}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Attachments;
--- src/components/UpdateDeleteCardModal.jsx
+++ src/components/UpdateDeleteCardModal.jsx
@@ -12,6 +12,7 @@ import toast from "react-hot-toast";
 import { getInitials } from "../utils/utilities";
 import UIImage from "../assets/Kanban_Board_UI.png";
 import Comments from "./Comments";
+import Attachments from "./Attachments";
 
 const UpdateDeleteCardModal = ({ updateDeleteCard, hideModal }) => {
   const { allUsers, setAllUsers, setRefresh, comments } = useContext(Context);
@@ -290,76 +291,7 @@ const UpdateDeleteCardModal = ({ updateDeleteCard, hideModal }) => {
       </div>
 
       {/* Attachment section */}
-      <div className="flex flex-col gap-y-3">
-        <div className="text-3xl font-semibold text-[color:var(--board-bg--color)]">
-          Attachments
-        </div>
-        <div className="grid grid-cols-3 gap-x-2 gap-y-4">
-          <div className="flex flex-row justify-between px-5 py-3 rounded-lg shadow-lg bg-gray-200">
-            <div className="w-[80%] overflow-hidden text-ellipsis line-clamp-1">
-              Attachment Attachment Attachment
-            </div>
-            <div className="flex flex-row items-center gap-x-2">
-              <a href={UIImage} download="Example">
-                <button
-                  className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
-                >
-                  {downloadIcon}
-                </button>
-              </a>
-
-              <button
-                onClick={() => alert("Attachment is deleted")}
-                className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
-              >
-                {deleteCommentIcon}
-              </button>
-            </div>
-          </div>
-
-          <div className="flex flex-row justify-between px-5 py-3 rounded-lg shadow-lg bg-gray-200">
-            <div className="w-[80%] overflow-hidden text-ellipsis line-clamp-1">
-              Attachment 2
-            </div>
-            <div className="flex flex-row items-center gap-x-2">
-              <a href={UIImage} download="Example">
-                <button
-                  className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
-                >
-                  {downloadIcon}
-                </button>
-              </a>
-              <button
-                onClick={() => alert("Attachment is deleted")}
-                className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
-              >
-                {deleteCommentIcon}
-              </button>
-            </div>
-          </div>
-
-          <div className="flex flex-row justify-between px-5 py-3 rounded-lg shadow-lg bg-gray-200">
-            <div className="w-[80%] overflow-hidden text-ellipsis line-clamp-1">
-              Attachment 3
-            </div>
-            <div className="flex flex-row items-center gap-x-2">
-              <a href={UIImage} download="Example">
-                <button
-                  className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
-                >
-                  {downloadIcon}
-                </button>
-              </a>
-              <button
-                onClick={() => alert("Attachment is deleted")}
-                className={`text-gray-500 hover:text-black transition ease-in-out duration-150`}
-              >
-                {deleteCommentIcon}
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Attachments></Attachments>
 
       {/* Comments Sections */}
       <Comments
