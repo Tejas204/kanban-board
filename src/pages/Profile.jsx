8b32fbf69f327866cf72b4fbafde5d025e33d944
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-04-06 23:06:39 +0200

    Redesign the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -10,31 +10,48 @@ const Profile = () => {
   return isLoading ? (
     <Loader />
   ) : (
-    <div className="flex h-fit mt-60 items-center justify-center">
-      <div className="flex flex-col gap-y-10 w-1/3 bg-[color:var(--card-bg--color)] shadow-xl rounded-lg text-[color:var(--primary-text--color)] text-xl pb-10">
-        <div className="text-center text-3xl font-semibold pt-3">
-          Your profile
+    // <div className="flex h-fit mt-60 items-center justify-center">
+    //   <div className="flex flex-col gap-y-10 w-1/3 bg-[color:var(--card-bg--color)] shadow-xl rounded-lg text-[color:var(--primary-text--color)] text-xl pb-10">
+    //     <div className="text-center text-3xl font-semibold pt-3">
+    //       Your profile
+    //     </div>
+    //     <div className="flex justify-center items-center text-center ">
+    //       <div className="flex justify-center items-center p-10 rounded-full w-24 h-24 font-bold text-center text-3xl bg-[color:var(--user-icon--bg-color--lavender)]">
+    //         {getInitials(user._id).initials}
+    //       </div>
+    //     </div>
+    //     <div className="grid grid-cols-2 gap-x-1 gap-y-5 px-3">
+    //       <div className="px-10 py-2 text-2xl">Name:</div>
+    //       <div className="px-10 py-2 text-2xl border-b-2 border-b-[color:var(--secondary-text--color)]">
+    //         {user.name}
+    //       </div>
+    //       <div className="px-10 py-2 text-2xl">Email:</div>
+    //       <div className="px-10 py-2 text-2xl border-b-2 border-b-[color:var(--secondary-text--color)]">
+    //         {user.email}
+    //       </div>
+    //       <div className="px-10 py-2 text-2xl">Joined us on:</div>
+    //       <div className="px-10 py-2 text-2xl border-b-2 border-b-[color:var(--secondary-text--color)]">
+    //         {user.createdAt}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className="flex flex-col gap-y-4 items-center">
+      <div className="flex flex-row gap-x-28 p-10 mt-10 items-center bg-[color:var(--card-bg--color)] w-[70%] rounded-lg shadow-xl">
+        {/* profile image */}
+        <div className="rounded-full bg-[color:var(--user-icon--bg-color--lavender)] p-14">
+          <p className="text-xl font-bold">TD</p>
         </div>
-        <div className="flex justify-center items-center text-center ">
-          <div className="flex justify-center items-center p-10 rounded-full w-24 h-24 font-bold text-center text-3xl bg-[color:var(--user-icon--bg-color--lavender)]">
-            {getInitials(user._id).initials}
-          </div>
-        </div>
-        <div className="grid grid-cols-2 gap-x-1 gap-y-5 px-3">
-          <div className="px-10 py-2 text-2xl">Name:</div>
-          <div className="px-10 py-2 text-2xl border-b-2 border-b-[color:var(--secondary-text--color)]">
-            {user.name}
-          </div>
-          <div className="px-10 py-2 text-2xl">Email:</div>
-          <div className="px-10 py-2 text-2xl border-b-2 border-b-[color:var(--secondary-text--color)]">
-            {user.email}
-          </div>
-          <div className="px-10 py-2 text-2xl">Joined us on:</div>
-          <div className="px-10 py-2 text-2xl border-b-2 border-b-[color:var(--secondary-text--color)]">
-            {user.createdAt}
-          </div>
+
+        {/* Profile information */}
+        <div className="flex flex-col gap-y-6 text-xl">
+          <div>Name:</div>
+          <div>Email:</div>
+          <div>Created at:</div>
         </div>
       </div>
+      <div></div>
     </div>
   );
 };
