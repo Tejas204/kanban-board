fb3fcf34e08bd1b9e016cbbecc1aa74a876d1b52
Tejas204 <tejasdhopavkar2000@gmail.com>
2025-04-09 22:24:04 +0200

    form to delete  kanban board

--- src/components/KanbanBoardSelectorModal.jsx
+++ src/components/KanbanBoardSelectorModal.jsx
@@ -35,55 +35,78 @@ const KanbanBoardSelectorModal = ({ hideModal }) => {
 
       {/* Tabs */}
       <div className="flex flex-col">
-        <div className="flex flex-row gap-x-10 text-xl text-[color:var(--board-bg--color)] border-b-2">
+        <div className="flex flex-row gap-x-6 text-xl text-[color:var(--board-bg--color)] border-b-2">
+          {/* Tab 1: New Kanban Board */}
           <button
             onClick={() => setBoardSelection("new")}
             className={`${
               boardSelection == "new"
                 ? "shadow-[inset_0_-4px_rgba(145,90,255)]"
-                : ""
+                : "text-gray-500"
             } pb-4`}
           >
             New kanban board
           </button>
+
+          {/* Tab 2: Select kanban board created by logged in user */}
           <button
             onClick={() => setBoardSelection("created")}
             className={`${
               boardSelection === "created"
                 ? "shadow-[inset_0_-4px_rgba(145,90,255)]"
-                : ""
+                : "text-gray-500"
             } pb-4`}
           >
             Created by me
           </button>
+
+          {/* Tab 3: Select kanban board shared with logged in user */}
           <button
             onClick={() => setBoardSelection("shared")}
             className={`${
               boardSelection === "shared"
                 ? "shadow-[inset_0_-4px_rgba(145,90,255)]"
-                : ""
+                : "text-gray-500"
             } pb-4`}
           >
             Shared with me
           </button>
+
+          {/* Tab 4: Delete kanban boards */}
+          <button
+            onClick={() => setBoardSelection("delete")}
+            className={`${
+              boardSelection === "delete"
+                ? "shadow-[inset_0_-4px_rgba(145,90,255)]"
+                : "text-gray-500"
+            } pb-4`}
+          >
+            Delete kanban boards
+          </button>
         </div>
       </div>
 
+      {/* Functionality */}
       <div className="flex flex-col gap-y-4">
         <p className="text-lg text-[color:var(--board-bg--color)]">
           {boardSelection === "created"
             ? "Choose a board created by you"
             : boardSelection === "new"
             ? "Create a new kanban board"
-            : "Choose a board shared with you"}
+            : boardSelection === "shared"
+            ? "Choose a board shared with you"
+            : "Delete boards"}
         </p>
         <form>
+          {/* Functionality 1: Enter name of new kanban board */}
           <input
             className={`${
               boardSelection === "new" ? "visible" : "hidden"
             } p-4 w-full border-[0.15rem] border-[color:var(--secondary-text--color)] text-[color:var(--card-bg--color)] bg-[color:var(--background-white)] text-lg rounded-md mb-2`}
             placeholder="Enter the name of the board"
           ></input>
+
+          {/* Functionality 2: Select from existing boards created by logged in user */}
           <select
             className={`${
               boardSelection == "created" ? "visible" : "hidden"
@@ -94,6 +117,7 @@ const KanbanBoardSelectorModal = ({ hideModal }) => {
             <option>Board 3</option>
           </select>
 
+          {/* Functionality 3: Select from kanban boards shared with logged in user */}
           <select
             className={`${
               boardSelection == "shared" ? "visible" : "hidden"
@@ -103,10 +127,29 @@ const KanbanBoardSelectorModal = ({ hideModal }) => {
             <option>Board 5</option>
             <option>Board 6</option>
           </select>
+
+          {/* Functionality 4: Delete the kanban boards created by logged in user */}
+          <div
+            className={`${
+              boardSelection === "delete" ? "visible" : "hidden"
+            } flex flex-row items-center gap-x-4`}
+          >
+            <input
+              type="checkbox"
+              name="board1"
+              className="h-4 w-4 accent-[color:var(--user-icon--bg-color--lavender)]"
+            ></input>
+            <label
+              for="board1"
+              className="text-[color:var(--board-bg--color)] text-lg"
+            >
+              Board 1
+            </label>
+          </div>
         </form>
       </div>
 
-      {/* Form */}
+      {/* Buttons */}
       <div className="flex flex-col">
         <form className="flex flex-col justify-center gap-y-10">
           <div className={`flex flex-row justify-start gap-x-10 `}>
